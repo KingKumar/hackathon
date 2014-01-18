cc41dbb9dadded8987ed7f00bb3d2636b39c4966
mcrazy20 <mcrazy300@yahoo.com>
2014-01-18 08:18:29 -0500

    Added instagram and merged the new layout and CSS

--- Javascript/node.js
+++ Javascript/node.js
@@ -3,9 +3,10 @@ var url = require('url');
 
 var twitter = require('ntwitter');
 
-var geocoderProvider = 'google';
+var geocoderProvider = 'openstreetmap';
 var httpAdapter = 'http';
 
+//Various keys to access the twitter API
 var twit = new twitter({
   consumer_key: 'wqERQjyGsZNzWZdbd9P6w',
   consumer_secret: 'l96ZBvNhjtbHx8j7npVBZNu5zzOwGPB386wdHtdxr0',
@@ -13,21 +14,46 @@ var twit = new twitter({
   access_token_secret: 'KdQXA1r3gVpqNnfy5uiHTNGGrf9sz5IeHXlNMwPVgCAsc'
 });
 
-
+//Used for Geocoder
 var extra = {
-	apiKey: 'AIzaSyAGMoM9QFBIxmUlmuIKn92HHt6NGL5lly8'
+	apiKey: 'AIzaSyCrOEigI-IexXXjOM7SAGKH2u8I9CAB05s'
 };
 
+//Allows us to get the Lat/Long of a location
 var geocoder = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter, extra);
 
-geocoder.geocode('Columbus Ohio', function(err, res){
+var ig = require('instagram-node').instagram();
+
+//Keys for instagram API
+ig.use({ client_id: 'fbc6b586a2f24eeb8876360c872a8f65',
+         client_secret: '2f4ba206c6b34c41a8676333324a1237' });
+
+//Call to the geocoder to get latitude and longitude
+geocoder.geocode('Columbus OH', function(err, res){
 	if (err)
 		console.log(err);
-	var location = (res[0]['longitude'] - 1) + ',' + (res[0]['latitude'] - 1) + ',' + (res[0]['longitude'] + 1) + ',' + (res[0]['latitude'] + 1)
+
+	//Giving a range to search for Twitter. Unfortunately the Twitter api doesn't allow you to use a radius when searching for geotagged tweets
+	var location = (res[0]['longitude'] - .2) + ',' + (res[0]['latitude'] - .2) + ',' + (parseFloat(res[0]['longitude'])
+	 + parseFloat(.2)) + ',' + (parseFloat(res[0]['latitude']) + parseFloat(.2));
+
 	console.log(location);
+
+	//Searching for instagram pictures
+	var loc = {};
+	loc.lat = parseFloat(res[0]['latitude']);
+	loc.lng = parseFloat(res[0]['longitude']);
+
+	//This function uses Latitude, Longitude, and a Distance and returns a list of pictures
+	ig.media_search(loc.lat, loc.lng, 3500, function(err, medias, limit){
+		if (err)
+			console.log(err);
+		console.log(medias);
+	});
+	//This is the twitter stream.
 	twit.stream('statuses/filter', {'locations': location}, function(stream) {
   		stream.on('data', function (data) {
-    		console.log(data);
+    		//console.log(data);
   		});
   		stream.on('error', function(response){
   			console.log(response);
@@ -35,7 +61,7 @@ geocoder.geocode('Columbus Ohio', function(err, res){
   		stream.on('destroy', function(response){
   			console.log(response);
   		});
-	setTimeout(stream.destroy, 5000);
+	setTimeout(stream.destroy, 50000);
 });
 
 });
